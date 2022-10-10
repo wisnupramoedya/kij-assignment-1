b3ded68e22a9d71380717c9a3e3c3d679cce0ebe
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2022-10-10 14:03:22 +0700

    feat(#2): add validation at front and back end

--- api/request/upload_encryption.py
+++ api/request/upload_encryption.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
-from wtforms import IntegerField, StringField, validators
-from wtforms.validators import DataRequired, NumberRange
+from wtforms import IntegerField, StringField
+from wtforms.validators import DataRequired, NumberRange, Length, ValidationError
+
+def FileSizeLimit(max_size_in_mb):
+    max_bytes = max_size_in_mb*1024*1024
+    def file_length_check(form, field):
+        if len(field.data.read()) > max_bytes:
+            raise ValidationError(f"File size must be less than {max_size_in_mb}MB")
+        field.data.seek(0)
+    return file_length_check
+
 
 class UploadEncryptionRequest(FlaskForm):
     class Meta:
@@ -12,8 +21,10 @@ class UploadEncryptionRequest(FlaskForm):
         NumberRange(min=1, max=3)
     ])
     key = StringField('key', validators=[
-        DataRequired()
+        DataRequired(),
+        Length(min=8, max=128)
     ])
     uploaded_file = FileField('uploaded_file', validators=[
-        FileRequired()
+        FileRequired(),
+        FileSizeLimit(max_size_in_mb=60)
     ])
